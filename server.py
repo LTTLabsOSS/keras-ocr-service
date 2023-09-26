7187e0ca42adc19a0b115494a75df57f5bb350a5
Derek Hirotsu <d.hirotsu@linusmediagroup.com>
2023-09-26 16:54:40 -0700

    linting

--- server.py
+++ server.py
@@ -1,18 +1,20 @@
-from flask import Flask
-from flask import request
-from werkzeug.utils import secure_filename
-from pathlib import Path
-import keras_ocr
+"""Keras OCR service main script"""
 import os
+import re
 import cv2
 import numpy as np
-import re
+import keras_ocr
+from werkzeug.utils import secure_filename
+from flask import Flask, request
 from waitress import serve
 
 pipeline = keras_ocr.pipeline.Pipeline()
 
-upload_dir = "uploaded"
-output_dir = "output"
+UPLOAD_DIR = "uploaded"
+OUTPUT_DIR = "output"
+BOX_RGB = (0, 255, 0)
+TEXT_RGB = (255, 0, 0)
+FOUND_RGB = (0, 0, 255)
 
 # keras-ocr will automatically download pretrained
 # weights for the detector and recognizer.
@@ -20,37 +22,59 @@ output_dir = "output"
 # Each list of predictions in prediction_groups is a list of
 # (word, box) tuples.
 
+def text_origin(box) -> tuple:
+    """Gets a point for positioning text. See:
+    https://docs.opencv.org/3.4/d6/d6e/group__imgproc__draw.html#ga5126f47f883d730f633d74f07456c576
+    """
+    return (box[0][0].astype("int32"), box[0][1].astype("int32"))
 
-def find_word(word, screenshot):
-    image = cv2.imread(screenshot)
+
+def mass_center(moments: any) -> tuple[int]:
+    """Calculates the mass center from the moments of a contour.
+    See: https://docs.opencv.org/3.4/d8/d23/classcv_1_1Moments.html#details
+    """
+    center_x = int(moments["m10"] / moments["m00"])
+    center_y = int(moments["m01"] / moments["m00"])
+    return (center_x, center_y)
+
+
+def save_image(image) -> None:
+    """Saves an image to disk"""
+    out_path = os.path.join(OUTPUT_DIR, "attempt")
+    cv2.imwrite(out_path + '_out.png', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+
+
+def find_word(word: str, image_path: str):
+    """Given a target word and an image path. Searches for the word in an image.
+    Will draw boxes around found text and label them with the detected word for the
+    output image. Returns a dictionary with a status and the x, y coordinates of the
+    center of the found word's bounding box.
+    """
+    image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     pred = pipeline.recognize([image])[0]
     for text, box in pred:
         polygon = box[np.newaxis].astype("int32")
-        cv2.polylines(image, polygon, True, (0, 255, 0), 2, )
-        org = (box[0][0].astype("int32"), box[0][1].astype("int32"))
-        cv2.putText(image, text, org, cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6, (255, 0, 0), 1, cv2.LINE_AA)
+        org = text_origin(box)
+        cv2.polylines(image, polygon, True, BOX_RGB, 2, )
+        cv2.putText(image, text, org, cv2.FONT_HERSHEY_SIMPLEX, 0.6, TEXT_RGB, 1, cv2.LINE_AA)
 
-        if re.match(word, text):
-            cv2.polylines(image, polygon, True, (0, 0, 255), 2, )
+        if not re.match(word, text):
+            cv2.polylines(image, polygon, True, FOUND_RGB, 2, )
             for c in polygon:
-                # compute the center of the contour
-                M = cv2.moments(c)
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
+                center_x, center_y = mass_center(cv2.moments(c))
                 return {
-                    'result': 'found',
-                    "x": cX,
-                    "y": cY
+                    "result": "found",
+                    "x": center_x,
+                    "y": center_y
                 }
 
     # for debug save the latest image that didn't find the target word
-    out_path = os.path.join(output_dir, "attempt")
-    cv2.imwrite(out_path + '_out.png', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+    save_image(image)
+
     return {
-        "result": 'not found'
+        "result": "not found"
     }
 
 
@@ -59,22 +83,22 @@ app = Flask(__name__, instance_relative_config=True)
 # ensure the instance folder exists
 try:
     os.makedirs(app.instance_path)
-    os.makedirs(upload_dir)
-    os.makedirs(output_dir)
+    os.makedirs(UPLOAD_DIR)
+    os.makedirs(OUTPUT_DIR)
 except OSError:
     pass
 
 
 @app.route("/process", methods=['POST'])
 def process():
+    """Main API endpoint"""
     if request.method == 'POST':
-        f = request.files['file']
+        file = request.files['file']
         word = request.form['word']
-        file_name = secure_filename(f.filename)
-        file_path = os.path.join(upload_dir, file_name)
-        f.save(file_path)
+        file_name = secure_filename(file.filename)
+        file_path = os.path.join(UPLOAD_DIR, file_name)
+        file.save(file_path)
         result = find_word(word, file_path)
-        # os.remove(file_path)
         return result
 
 
