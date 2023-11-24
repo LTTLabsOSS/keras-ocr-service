f7653f27f39b64648fa5482b92b0b944973bb7c5
Nikolas <nikolas@linusmediagroup.com>
2023-11-24 11:06:27 -0800

    add default path

--- display_latest_upload.py
+++ display_latest_upload.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
     upload_dir = os.path.join(script_dir, "uploaded")
     nothing_to_display = os.path.join(script_dir, "images", "nothing_to_display.png")
-    previous_latest_image = cv2.resize(cv2.imread(nothing_to_display), (1280, 720))
+    nothing_to_display_image = cv2.resize(cv2.imread(nothing_to_display), (1280, 720))
+    previous_latest_image = nothing_to_display_image
     while True:
         latest_image_path = get_latest_image(upload_dir)
         if latest_image_path:
@@ -25,6 +26,8 @@ if __name__ == "__main__":
             cv2.imshow("latest upload", resized_image)
         elif previous_latest_image is not None:
             cv2.imshow("latest upload", previous_latest_image)
+        else: 
+            cv2.imshow("latest upload", nothing_to_display_image)
         time.sleep(0.05)
         cv2.waitKey(1)
 
